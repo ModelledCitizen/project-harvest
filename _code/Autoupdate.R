106a09395e88f8e5f61fe5883a87ab9b94fcd2a7
ModelledCitizen <50756646+ModelledCitizen@users.noreply.github.com>
2020-03-18 09:02:16 -0400

    add autoupdate

---
+++ _code/Autoupdate.R
@@ -0,0 +1,29 @@
+#      __  __            __   _               __          __
+#     / / / /___  ____  / /__(_)___  _____   / /   ____ _/ /_
+#    / /_/ / __ \/ __ \/ //_/ / __ \/ ___/  / /   / __ `/ __ \
+#   / __  / /_/ / /_/ / ,< / / / / (__  )  / /___/ /_/ / /_/ /
+#  /_/ /_/\____/ .___/_/|_/_/_/ /_/____/  /_____/\__,_/_.___/
+#             /_/
+#
+#   Title: Automatic Updates
+#  Author: UnlikelyVolcano
+# Updated: 18 March 2020
+#   Notes:
+#
+
+# Working Directory -------------------------------------------------------
+
+setwd("~/project-harvest")
+
+
+# Update Data -------------------------------------------------------------
+
+source("_code/RCP_Polls.R")
+
+source("_code/NYT_Results.R")
+
+
+# Push to Repo ------------------------------------------------------------
+
+system("git add .; git commit -m 'Automatic update'")
+#system("git push")
